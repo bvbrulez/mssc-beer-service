e4222a39f420984b936bad9a2fb782e3a3cebd7a
Niemann, Markus <markus.niemann@lhind.dlh.de>
2020-12-26 17:47:42 +0100

    feat: add data model. Closes #2

---
+++ src/main/java/com/mniemann/udemy/springboot/msscbeerservice/beer/BeerStyleEnum.java
@@ -0,0 +1,14 @@
+package com.mniemann.udemy.springboot.msscbeerservice.beer;
+
+public enum BeerStyleEnum {
+  LAGER,
+  PILSNER,
+  STOUT,
+  GOSE,
+  PORTER,
+  ALE,
+  WHEAT,
+  IPA,
+  PALE_ALE,
+  SAISON
+}
